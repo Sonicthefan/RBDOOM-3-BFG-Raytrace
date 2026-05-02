48ee632af52cc3eea87c6a50e4692572d54a47ea
lizardmech <lizardmech@gmail.com>
2026-05-02 17:13:16 +1000

    Extract path trace scene capture helpers

--- neo/renderer/NVRHI/PathTracePrimaryPass.cpp
+++ neo/renderer/NVRHI/PathTracePrimaryPass.cpp
@@ -6,6 +6,7 @@
 #include "PathTraceEmissiveCandidates.h"
 #include "PathTraceGuiSurfaces.h"
 #include "PathTracePrimaryPass.h"
+#include "PathTraceSceneCapture.h"
 #include "PathTraceSkinning.h"
 #include "PathTraceTextureRegistry.h"
 #include "../RenderCommon.h"
@@ -603,22 +604,6 @@ struct RtSmokeSurfaceClassStats
     int unknownTriangles = 0;
 };
 
-struct RtSmokeSurfaceSkipStats
-{
-    int nullSurface = 0;
-    int missingGeometry = 0;
-    int nullMaterial = 0;
-    int nullSpace = 0;
-    int nullModel = 0;
-    int invalidIndexCount = 0;
-    int nonCurrentCache = 0;
-    int limitExceeded = 0;
-    int zeroAreaOnly = 0;
-    int emptyClassBuffer = 0;
-    int guiSurface = 0;
-    int callbackEntity = 0;
-};
-
 struct RtSmokeDynamicGeometryStats
 {
     int rigidSurfaces = 0;
@@ -633,20 +618,6 @@ struct RtSmokeDynamicGeometryStats
     int skinnedRtCpuSkinnedIndexes = 0;
     int particleAlphaIndexes = 0;
     int unknownIndexes = 0;
-};
-
-struct RtSmokeAttributeClassStats
-{
-    int invalidNormalVerts = 0;
-    int invalidNormalTriangles = 0;
-    int invalidUvVerts = 0;
-    int invalidUvTriangles = 0;
-    int forcedGeometricNormalTriangles = 0;
-};
-
-struct RtSmokeAttributeStats
-{
-    RtSmokeAttributeClassStats classes[RT_SMOKE_CLASS_COUNT];
 };
 
 struct RtSmokeMaterialSample
@@ -2590,136 +2561,6 @@ void AddSmokeTranslucentDebugSample(RtSmokeMaterialStats& stats, const drawSurf_
     sample.info = BuildSmokeTranslucentClassifierInfo(material);
 }
 
-void TransformSurfacePointToWorld(const drawSurf_t* drawSurf, const idVec3& localPoint, idVec3& worldPoint)
-{
-    if (drawSurf->space)
-    {
-        R_LocalPointToGlobal(drawSurf->space->modelMatrix, localPoint, worldPoint);
-        return;
-    }
-
-    worldPoint = localPoint;
-}
-
-void TransformSurfaceVectorToWorld(const drawSurf_t* drawSurf, const idVec3& localVector, idVec3& worldVector)
-{
-    if (drawSurf->space)
-    {
-        R_LocalVectorToGlobal(drawSurf->space->modelMatrix, localVector, worldVector);
-        return;
-    }
-
-    worldVector = localVector;
-}
-
-bool ValidateSmokeDrawSurface(const viewDef_t* viewDef, const drawSurf_t* drawSurf, const srfTriangles_t*& tri, RtSmokeSurfaceSkipStats* skipStats)
-{
-    tri = nullptr;
-    if (!drawSurf)
-    {
-        if (skipStats)
-        {
-            ++skipStats->nullSurface;
-        }
-        return false;
-    }
-
-    if (!drawSurf->frontEndGeo)
-    {
-        if (skipStats)
-        {
-            ++skipStats->missingGeometry;
-        }
-        return false;
-    }
-
-    if (!drawSurf->material)
-    {
-        if (skipStats)
-        {
-            ++skipStats->nullMaterial;
-        }
-        return false;
-    }
-
-    const bool guiDrawSurface = IsSmokeGuiDrawSurface(drawSurf);
-    if (guiDrawSurface && r_pathTracingAllowGuiSurfaces.GetInteger() == 0)
-    {
-        if (skipStats)
-        {
-            ++skipStats->guiSurface;
-        }
-        return false;
-    }
-
-    if (!drawSurf->space)
-    {
-        if (skipStats)
-        {
-            ++skipStats->nullSpace;
-        }
-        return false;
-    }
-
-    const viewEntity_t* space = drawSurf->space;
-    const idRenderEntityLocal* entityDef = space->entityDef;
-    const renderEntity_t* renderEntity = entityDef ? &entityDef->parms : nullptr;
-    if (!guiDrawSurface && viewDef && space != &viewDef->worldSpace && (!renderEntity || !renderEntity->hModel))
-    {
-        if (skipStats)
-        {
-            ++skipStats->nullModel;
-        }
-        return false;
-    }
-
-    const bool riskyCallbackSurface =
-        renderEntity &&
-        renderEntity->callback &&
-        renderEntity->customShader != nullptr;
-    if (!guiDrawSurface && riskyCallbackSurface && r_pathTracingSkipCallbackEntities.GetInteger() != 0)
-    {
-        if (skipStats)
-        {
-            ++skipStats->callbackEntity;
-        }
-        return false;
-    }
-
-    tri = drawSurf->frontEndGeo;
-    if (!tri->verts || !tri->indexes || tri->numVerts < 3 || tri->numIndexes < 3)
-    {
-        if (skipStats)
-        {
-            ++skipStats->missingGeometry;
-        }
-        return false;
-    }
-
-    if ((tri->numIndexes % 3) != 0 || drawSurf->numIndexes < 3)
-    {
-        if (skipStats)
-        {
-            ++skipStats->invalidIndexCount;
-        }
-        return false;
-    }
-
-    const bool hasAmbientCache = tri->ambientCache != 0;
-    const bool hasIndexCache = tri->indexCache != 0;
-    if ((hasAmbientCache && !vertexCache.CacheIsCurrent(tri->ambientCache)) ||
-        (hasIndexCache && !vertexCache.CacheIsCurrent(tri->indexCache)))
-    {
-        if (skipStats)
-        {
-            ++skipStats->nonCurrentCache;
-        }
-        return false;
-    }
-
-    return true;
-}
-
 RtSmokeSurfaceClass ClassifySmokeSurface(const viewDef_t* viewDef, const drawSurf_t* drawSurf, const srfTriangles_t* tri)
 {
     const viewEntity_t* space = drawSurf ? drawSurf->space : nullptr;
@@ -4001,9 +3842,6 @@ uint64 BuildSmokeStaticSurfaceKey(const drawSurf_t* drawSurf, const srfTriangles
     return hash;
 }
 
-void TransformSmokeSurfaceVertexToWorld(const drawSurf_t* drawSurf, const srfTriangles_t* tri, int vertexIndex, const idJointMat* rtCpuSkinningJoints, idVec3& worldPosition);
-PathTraceSmokeVertex BuildSmokeSurfaceVertex(const drawSurf_t* drawSurf, const srfTriangles_t* tri, int vertexIndex, const idJointMat* rtCpuSkinningJoints);
-
 bool FindCenterCameraRayAnchor(const viewDef_t* viewDef, idVec3& anchorPoint, int& anchorSurface, int& anchorTriangle)
 {
     const idVec3 rayOrigin = viewDef->renderView.vieworg;
@@ -4411,153 +4249,6 @@ void LogSmokeGuiSurfaceDump(const viewDef_t* viewDef, const RtSmokeMaterialTable
         logged);
 }
 
-PathTraceSmokeVertex BuildSmokeSurfaceVertex(const drawSurf_t* drawSurf, const srfTriangles_t* tri, int vertexIndex, const idJointMat* rtCpuSkinningJoints)
-{
-    const idDrawVert& drawVert = tri->verts[vertexIndex];
-    idVec3 localPosition = drawVert.xyz;
-    idVec3 localNormal = drawVert.GetNormal();
-    if (rtCpuSkinningJoints)
-    {
-        localPosition = TransformSmokeSkinnedVertexPosition(drawVert, rtCpuSkinningJoints);
-        localNormal = TransformSmokeSkinnedVertexNormal(drawVert, rtCpuSkinningJoints);
-    }
-
-    idVec3 worldPosition;
-    idVec3 worldNormal;
-    TransformSurfacePointToWorld(drawSurf, localPosition, worldPosition);
-    TransformSurfaceVectorToWorld(drawSurf, localNormal, worldNormal);
-    worldNormal.Normalize();
-
-    const idVec2 texCoord = drawVert.GetTexCoord();
-    PathTraceSmokeVertex vertex = {};
-    vertex.position[0] = worldPosition.x;
-    vertex.position[1] = worldPosition.y;
-    vertex.position[2] = worldPosition.z;
-    vertex.position[3] = 1.0f;
-    vertex.normal[0] = worldNormal.x;
-    vertex.normal[1] = worldNormal.y;
-    vertex.normal[2] = worldNormal.z;
-    vertex.normal[3] = 0.0f;
-    vertex.texCoord[0] = texCoord.x;
-    vertex.texCoord[1] = texCoord.y;
-    vertex.texCoord[2] = 0.0f;
-    vertex.texCoord[3] = 0.0f;
-    vertex.color[0] = drawVert.color[0] * (1.0f / 255.0f);
-    vertex.color[1] = drawVert.color[1] * (1.0f / 255.0f);
-    vertex.color[2] = drawVert.color[2] * (1.0f / 255.0f);
-    vertex.color[3] = drawVert.color[3] * (1.0f / 255.0f);
-    vertex.color2[0] = drawVert.color2[0] * (1.0f / 255.0f);
-    vertex.color2[1] = drawVert.color2[1] * (1.0f / 255.0f);
-    vertex.color2[2] = drawVert.color2[2] * (1.0f / 255.0f);
-    vertex.color2[3] = drawVert.color2[3] * (1.0f / 255.0f);
-    return vertex;
-}
-
-void TransformSmokeSurfaceVertexToWorld(const drawSurf_t* drawSurf, const srfTriangles_t* tri, int vertexIndex, const idJointMat* rtCpuSkinningJoints, idVec3& worldPosition)
-{
-    const PathTraceSmokeVertex vertex = BuildSmokeSurfaceVertex(drawSurf, tri, vertexIndex, rtCpuSkinningJoints);
-    worldPosition.Set(vertex.position[0], vertex.position[1], vertex.position[2]);
-}
-
-int AppendSmokeSurfaceGeometry(const drawSurf_t* drawSurf, const srfTriangles_t* tri, uint32_t surfaceClassId, uint32_t materialId, std::vector<PathTraceSmokeVertex>& vertices, std::vector<uint32_t>& indexes, std::vector<uint32_t>& triangleClasses, std::vector<uint32_t>& triangleMaterials, RtSmokeSurfaceSkipStats& skipStats, RtSmokeAttributeStats& attributeStats)
-{
-    const size_t vertexStart = vertices.size();
-    const size_t indexStart = indexes.size();
-    const size_t classStart = triangleClasses.size();
-    const size_t materialStart = triangleMaterials.size();
-    const uint32_t indexBase = static_cast<uint32_t>(vertices.size());
-    const idJointMat* rtCpuSkinningJoints = GetSmokeRtCpuSkinningJoints(tri);
-    const int classIndex = idMath::ClampInt(0, RT_SMOKE_CLASS_COUNT - 1, static_cast<int>(surfaceClassId & RT_SMOKE_TRIANGLE_CLASS_MASK));
-
-    for (int vertexIndex = 0; vertexIndex < tri->numVerts; ++vertexIndex)
-    {
-        PathTraceSmokeVertex vertex = BuildSmokeSurfaceVertex(drawSurf, tri, vertexIndex, rtCpuSkinningJoints);
-        const idVec3 normal = SmokeVertexNormal(vertex);
-        const idVec2 texCoord = SmokeVertexTexCoord(vertex);
-        if (!SmokeNormalIsUsable(normal))
-        {
-            ++attributeStats.classes[classIndex].invalidNormalVerts;
-            vertex.normal[0] = 0.0f;
-            vertex.normal[1] = 0.0f;
-            vertex.normal[2] = 0.0f;
-        }
-        if (!SmokeTexCoordIsUsable(texCoord))
-        {
-            ++attributeStats.classes[classIndex].invalidUvVerts;
-            vertex.texCoord[0] = 0.0f;
-            vertex.texCoord[1] = 0.0f;
-        }
-        vertices.push_back(vertex);
-    }
-
-    for (int sourceIndex = 0; sourceIndex + 2 < tri->numIndexes; sourceIndex += 3)
-    {
-        const int i0 = tri->indexes[sourceIndex + 0];
-        const int i1 = tri->indexes[sourceIndex + 1];
-        const int i2 = tri->indexes[sourceIndex + 2];
-        if (i0 < 0 || i1 < 0 || i2 < 0 || i0 >= tri->numVerts || i1 >= tri->numVerts || i2 >= tri->numVerts)
-        {
-            ++skipStats.invalidIndexCount;
-            continue;
-        }
-
-        const PathTraceSmokeVertex& v0 = vertices[indexBase + static_cast<uint32_t>(i0)];
-        const PathTraceSmokeVertex& v1 = vertices[indexBase + static_cast<uint32_t>(i1)];
-        const PathTraceSmokeVertex& v2 = vertices[indexBase + static_cast<uint32_t>(i2)];
-        const idVec3 p0 = SmokeVertexPosition(v0);
-        const idVec3 p1 = SmokeVertexPosition(v1);
-        const idVec3 p2 = SmokeVertexPosition(v2);
-        if (IsZeroAreaSmokeTriangle(p0, p1, p2))
-        {
-            continue;
-        }
-
-        indexes.push_back(indexBase + static_cast<uint32_t>(i0));
-        indexes.push_back(indexBase + static_cast<uint32_t>(i1));
-        indexes.push_back(indexBase + static_cast<uint32_t>(i2));
-        const bool invalidNormalTriangle =
-            !SmokeNormalIsUsable(SmokeVertexNormal(v0)) ||
-            !SmokeNormalIsUsable(SmokeVertexNormal(v1)) ||
-            !SmokeNormalIsUsable(SmokeVertexNormal(v2));
-        const bool invalidUvTriangle =
-            !SmokeTexCoordIsUsable(SmokeVertexTexCoord(v0)) ||
-            !SmokeTexCoordIsUsable(SmokeVertexTexCoord(v1)) ||
-            !SmokeTexCoordIsUsable(SmokeVertexTexCoord(v2));
-        const bool preferGeometricNormal =
-            invalidNormalTriangle ||
-            static_cast<RtSmokeSurfaceClass>(classIndex) == RtSmokeSurfaceClass::ParticleAlpha;
-
-        if (invalidNormalTriangle)
-        {
-            ++attributeStats.classes[classIndex].invalidNormalTriangles;
-        }
-        if (invalidUvTriangle)
-        {
-            ++attributeStats.classes[classIndex].invalidUvTriangles;
-        }
-        if (preferGeometricNormal)
-        {
-            ++attributeStats.classes[classIndex].forcedGeometricNormalTriangles;
-        }
-
-        triangleClasses.push_back(surfaceClassId | (preferGeometricNormal ? RT_SMOKE_TRIANGLE_FORCE_GEOMETRIC_NORMAL : 0u));
-        triangleMaterials.push_back(materialId);
-    }
-
-    const int emittedIndexes = static_cast<int>(indexes.size() - indexStart);
-    if (emittedIndexes <= 0 || triangleClasses.size() == classStart || triangleMaterials.size() == materialStart)
-    {
-        vertices.resize(vertexStart);
-        indexes.resize(indexStart);
-        triangleClasses.resize(classStart);
-        triangleMaterials.resize(materialStart);
-        ++skipStats.zeroAreaOnly;
-        return 0;
-    }
-
-    return emittedIndexes;
-}
-
 bool CaptureDoomSurfacesForSmokeTest(const viewDef_t* viewDef, std::vector<PathTraceSmokeVertex>& vertexData, std::vector<uint32_t>& indexData, std::vector<uint32_t>& triangleClassData, std::vector<uint32_t>& triangleMaterialData, std::vector<uint64>& staticSurfaceKeys, std::vector<PathTraceSmokeVertex>& staticVertexCache, std::vector<uint32_t>& staticIndexCache, std::vector<uint32_t>& staticTriangleClassCache, std::vector<uint32_t>& staticTriangleMaterialCache, bool& staticCacheChanged, idVec3& sceneOrigin, int& sourceSurfaces, int& sourceVerts, int& sourceIndexes, int& anchorTriangle, RtSmokeSurfaceClassStats& classStats, RtSmokeSurfaceSkipStats& skipStats, RtSmokeDynamicGeometryStats& dynamicStats, RtSmokeAttributeStats& attributeStats, RtSmokeMaterialStats& materialStats, RtSmokeBucketRanges& bucketRanges, RtSmokeSurfaceClassReasonSamples* reasonSamples)
 {
     sourceSurfaces = 0;
@@ -4657,7 +4348,21 @@ bool CaptureDoomSurfacesForSmokeTest(const viewDef_t* viewDef, std::vector<PathT
             continue;
         }
 
-        const int emittedIndexes = AppendSmokeSurfaceGeometry(drawSurf, tri, surfaceClassId, materialId, staticVertexCache, staticIndexCache, staticTriangleClassCache, staticTriangleMaterialCache, skipStats, attributeStats);
+        const int emittedIndexes = AppendSmokeSurfaceGeometry(
+            drawSurf,
+            tri,
+            surfaceClassId,
+            materialId,
+            RT_SMOKE_CLASS_COUNT,
+            RT_SMOKE_TRIANGLE_CLASS_MASK,
+            static_cast<uint32_t>(RtSmokeSurfaceClass::ParticleAlpha),
+            RT_SMOKE_TRIANGLE_FORCE_GEOMETRIC_NORMAL,
+            staticVertexCache,
+            staticIndexCache,
+            staticTriangleClassCache,
+            staticTriangleMaterialCache,
+            skipStats,
+            attributeStats);
         if (emittedIndexes <= 0)
         {
             continue;
@@ -4714,7 +4419,21 @@ bool CaptureDoomSurfacesForSmokeTest(const viewDef_t* viewDef, std::vector<PathT
         std::vector<uint32_t>& bucketIndexes = bucketIndexData[bucketIndex];
         std::vector<uint32_t>& bucketClasses = bucketTriangleClassData[bucketIndex];
         std::vector<uint32_t>& bucketMaterials = bucketTriangleMaterialData[bucketIndex];
-        const int emittedIndexes = AppendSmokeSurfaceGeometry(drawSurf, tri, surfaceClassId, materialId, bucketVertices, bucketIndexes, bucketClasses, bucketMaterials, skipStats, attributeStats);
+        const int emittedIndexes = AppendSmokeSurfaceGeometry(
+            drawSurf,
+            tri,
+            surfaceClassId,
+            materialId,
+            RT_SMOKE_CLASS_COUNT,
+            RT_SMOKE_TRIANGLE_CLASS_MASK,
+            static_cast<uint32_t>(RtSmokeSurfaceClass::ParticleAlpha),
+            RT_SMOKE_TRIANGLE_FORCE_GEOMETRIC_NORMAL,
+            bucketVertices,
+            bucketIndexes,
+            bucketClasses,
+            bucketMaterials,
+            skipStats,
+            attributeStats);
         if (emittedIndexes <= 0)
         {
             continue;
---
+++ neo/renderer/NVRHI/PathTraceSceneCapture.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "PathTraceGeometry.h"
+
+#include <vector>
+
+struct drawSurf_t;
+struct srfTriangles_t;
+struct viewDef_t;
+class idJointMat;
+
+struct RtSmokeSurfaceSkipStats
+{
+    int nullSurface = 0;
+    int missingGeometry = 0;
+    int nullMaterial = 0;
+    int nullSpace = 0;
+    int nullModel = 0;
+    int invalidIndexCount = 0;
+    int nonCurrentCache = 0;
+    int limitExceeded = 0;
+    int zeroAreaOnly = 0;
+    int emptyClassBuffer = 0;
+    int guiSurface = 0;
+    int callbackEntity = 0;
+};
+
+struct RtSmokeAttributeClassStats
+{
+    int invalidNormalVerts = 0;
+    int invalidNormalTriangles = 0;
+    int invalidUvVerts = 0;
+    int invalidUvTriangles = 0;
+    int forcedGeometricNormalTriangles = 0;
+};
+
+struct RtSmokeAttributeStats
+{
+    RtSmokeAttributeClassStats classes[5];
+};
+
+void TransformSurfacePointToWorld(const drawSurf_t* drawSurf, const idVec3& localPoint, idVec3& worldPoint);
+void TransformSurfaceVectorToWorld(const drawSurf_t* drawSurf, const idVec3& localVector, idVec3& worldVector);
+bool ValidateSmokeDrawSurface(const viewDef_t* viewDef, const drawSurf_t* drawSurf, const srfTriangles_t*& tri, RtSmokeSurfaceSkipStats* skipStats);
+PathTraceSmokeVertex BuildSmokeSurfaceVertex(const drawSurf_t* drawSurf, const srfTriangles_t* tri, int vertexIndex, const idJointMat* rtCpuSkinningJoints);
+void TransformSmokeSurfaceVertexToWorld(const drawSurf_t* drawSurf, const srfTriangles_t* tri, int vertexIndex, const idJointMat* rtCpuSkinningJoints, idVec3& worldPosition);
+int AppendSmokeSurfaceGeometry(
+    const drawSurf_t* drawSurf,
+    const srfTriangles_t* tri,
+    uint32_t surfaceClassId,
+    uint32_t materialId,
+    int classCount,
+    uint32_t triangleClassMask,
+    uint32_t particleAlphaClassId,
+    uint32_t forceGeometricNormalFlag,
+    std::vector<PathTraceSmokeVertex>& vertices,
+    std::vector<uint32_t>& indexes,
+    std::vector<uint32_t>& triangleClasses,
+    std::vector<uint32_t>& triangleMaterials,
+    RtSmokeSurfaceSkipStats& skipStats,
+    RtSmokeAttributeStats& attributeStats);
