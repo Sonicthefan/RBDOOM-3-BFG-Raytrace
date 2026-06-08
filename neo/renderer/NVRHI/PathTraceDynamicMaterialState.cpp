e101ea0b25ed66e4084bd1e2c965b8485d90dd28
lizardmech <lizardmech@gmail.com>
2026-06-08 18:35:30 +1000

    Refresh matclass record from live material info

--- neo/renderer/NVRHI/PathTraceDynamicMaterialState.cpp
+++ neo/renderer/NVRHI/PathTraceDynamicMaterialState.cpp
@@ -210,11 +210,15 @@ uint32_t AddSmokeMaterialTableEntry(RtSmokeMaterialTableBuild& table, uint32_t m
     }
     if (r_pathTracingMatClassEnable.GetInteger() != 0)
     {
-        const RtMaterialRecord* materialClassRecord = FindPathTraceMaterialRecord(materialId);
-        if ((!materialClassRecord || !materialClassRecord->valid) && !info.materialName.IsEmpty())
+        const RtMaterialRecord* materialClassRecord = nullptr;
+        if (!info.materialName.IsEmpty())
         {
             const idMaterial* materialDecl = declManager ? declManager->FindMaterial(info.materialName.c_str(), false) : nullptr;
             materialClassRecord = &RegisterPathTraceMaterialRecord(materialDecl, info);
+        }
+        else
+        {
+            materialClassRecord = FindPathTraceMaterialRecord(materialId);
         }
         if (materialClassRecord && materialClassRecord->valid)
         {
